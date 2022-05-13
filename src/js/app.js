4233dc0cf66547c66a45645d46973c57ceb990da
LLEEVV2020 <lleevv2020@gmail.com>
2022-05-13 15:11:51 +0400

    add

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,9 @@
+import '../../node_modules/@popperjs/core/dist/umd/popper.min.js';
+import '../../node_modules/bootstrap/dist/js/bootstrap.esm.js';
+
 import * as flsFunctions from "./modules/functions.js";
 
 flsFunctions.isWebp();
-
-
 
 // core version + navigation, pagination modules:
 import Swiper, {
